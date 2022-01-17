04bf818fd438a1d31d968e2200e11b0b4080db3d
Brian McClune <bpmcclune@gmail.com>
2022-01-17 13:36:44 -0500

    Start adding basic CSS grid to base layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
       sans: ['Noto Sans', 'sans-serif'],
       serif: ['Noto Serif', 'serif'],
     },
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'mobile': '1fr',
+        'two': '1fr 3fr',
+      },
+      gridTemplateRows: {
+        'mobile': 'repeat(3, auto) max-content 1fr',
+        'four': 'repeat(2, auto) max-content 1fr',
+      }
+    },
   },
   plugins: [],
 }
