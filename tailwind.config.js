da5374121668b74de37f4602b0c44b513ca9c91e
Brian McClune <bpmcclune@gmail.com>
2022-01-17 18:35:23 -0500

    Adjust grid settings to expand main to win height

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
         'two': '1fr 3fr',
       },
       gridTemplateRows: {
-        'mobile': 'repeat(3, auto) max-content 1fr',
-        'four': 'repeat(2, auto) max-content 1fr',
+        'mobile': 'repeat(3, auto) 1fr auto',
+        'four': 'repeat(2, auto) 1fr auto',
       }
     },
   },
