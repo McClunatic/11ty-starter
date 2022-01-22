65a12f2cf17fdbd88666bf3919887f56c618444c
Brian McClune <bpmcclune@gmail.com>
2022-01-22 17:43:10 -0500

    Add eleventy-plugin-toc to package, .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const navigation = require("@11ty/eleventy-navigation")
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const toc = require("eleventy-plugin-toc")
+const md = require("markdown-it")().use(require("markdown-it-anchor"))
 
 module.exports = function(eleventyConfig) {
 
@@ -12,8 +14,11 @@ module.exports = function(eleventyConfig) {
   const prismDst = 'css/prismjs'
   eleventyConfig.addPassthroughCopy({[prismSrc]: prismDst})
 
+  eleventyConfig.setLibrary('md', md)
+
   eleventyConfig.addPlugin(navigation)
   eleventyConfig.addPlugin(syntaxHighlight)
+  eleventyConfig.addPlugin(toc)
 
   return {
     dir: {
