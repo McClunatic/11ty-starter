c3aef9ce50e17fa2f02d89062d619143659e8429
Brian McClune <bpmcclune@gmail.com>
2022-01-24 20:11:07 -0500

    Move log matching function into lunr module

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,4 @@
-import { collection, idx } from './modules/lunr-idx.js'
+import logMatches from './modules/lunr-idx.js'
 
 const form = document.querySelector('form')
-form.addEventListener('submit', function(event) {
-  event.preventDefault()
-  const input = event.target.children['site-search']
-  const results = idx.search(`content:${input.value}`)
-  results.forEach(res => {
-    for ( const [key, value] of Object.entries(res.matchData.metadata)) {
-      const url = collection[res.ref].url
-      for (const [start, end] of value.content.position) {
-        console.log(`${key}: ${url}: ...${collection[res.ref].content.slice(
-          Math.max(0, start - 20),
-          start + end + 20,
-        )}...`)
-      }
-    }
-  })
-  input.value = ''
-})
+form.addEventListener('submit', logMatches)
--- src/js/modules/lunr-idx.js
+++ src/js/modules/lunr-idx.js
@@ -21,4 +21,22 @@ const idx = lunr(function() {
   }
 })
 
-export { collection, idx }
+function logMatches(event) {
+  event.preventDefault()
+  const input = event.target.children['site-search']
+  const results = idx.search(`content:${input.value}`)
+  results.forEach(res => {
+    for ( const [key, value] of Object.entries(res.matchData.metadata)) {
+      const url = collection[res.ref].url
+      for (const [start, end] of value.content.position) {
+        console.log(`${key}: ${url}: ...${collection[res.ref].content.slice(
+          Math.max(0, start - 20),
+          start + end + 20,
+        )}...`)
+      }
+    }
+  })
+  input.value = ''
+}
+
+export default logMatches
